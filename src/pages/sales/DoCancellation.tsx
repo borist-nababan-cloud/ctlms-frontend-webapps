8e990c09fb9b70bf6d2e23d617ac8040db4788d4
Borist Nababan <borist.nababan@gmail.com>
2026-07-12 15:21:58 +0700

    fix: unused variables in DoCancellation

--- src/pages/sales/DoCancellation.tsx
+++ src/pages/sales/DoCancellation.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import {
     Box,
     Button,
@@ -75,7 +75,7 @@ const DoCancellation = () => {
         
         try {
             setActionLoading(true);
-            const data = await doCancellationService.updateRequestStatus(selectedRequest.id, confirmAction, profile.uuid);
+            await doCancellationService.updateRequestStatus(selectedRequest.id, confirmAction, profile.uuid);
             
             if (confirmAction === 'APPROVED') {
                 setSuccessMsg('Data berhasil diupdate');
